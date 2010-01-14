110c278cbe2b8038e1afa1db2e84d943678bc653
Kristoffer Walker <kixxauth@gmail.com>
2010-01-14 06:26:07 -0500

    Refactored gate to setup for user levels
    The structure of groups.yaml and gate module needed to be changed to
    get ready for the implementation of user permission levels.
    
     On branch master
    	modified:   gae_py/gate.py
    	modified:   gae_py/groups.yaml

--- gae_py/gate.py
+++ gae_py/gate.py
@@ -24,27 +24,49 @@ groups = yaml.load(
         os.path.dirname(os.path.abspath(__file__)), 'groups.yaml')))
 # todo: Should the 'groups' global be private?
 
-def get_builder(username, permissions, interface):
-  """Takes a username, group membership list, and interface name and returns
-  the named builder function from the factory module if and only if there is a
-  group listing for the interface in groups.yaml, the interface factory exists
-  in the factory module, and the given group has permission on the given
-  interface as determined by groups.yaml.
+def get_builder(username, user_groups, interface):
+  """Check user capabilities and build an datastore interface function.
+
+  Takes a username, group membership list, and interface name and returns the
+  named builder function from the factory module if and only if 3 conditions
+  are met:
+  
+  1. There is a group listing for the interface in groups.yaml. 
+  2. The interface factory exists in the factory module. 
+  3. The given group has permission on the given interface as determined by
+  groups.yaml.
+  
+  Args:
+    username: User name string.
+    user_groups: List of groups the user belongs to.
+    interface: Datastore interface to build.
+
+  Returns:
+    An interface function if all the conditions are met, or None if not.
   """
-  g = groups.get(interface)
-  if g is None:
-    logging.warn(('gate.get_builder(): '
-      'There is no groups config for interface "%s".'), interface)
-    return None
-  for p in permissions:
-    if p in g:
+  builder = None
+  level = 0
+
+  for g in user_groups:
+    group = groups.get(g)
+    if group['level'] > level:
+      level = group['level']
+
+    if group is None:
+      logging.warn('gate.get_builder(): There is no group config for "%s".', g)
+      return None
+
+    if interface in group['interfaces']:
       builder = getattr(factory, interface, None)
       if builder is None:
         logging.warn(('gate.get_builder(): '
           'There is no builder for interface "%s".'), interface)
         return None
-      return builder(username, permissions)
+
+  if callable(builder):
+    return builder(username, level)
 
   logging.warn(('gate.get_builder(): '
     'permission denied for interface "%s".'), interface)
   return None
+
