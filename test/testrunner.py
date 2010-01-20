60c81b2562d97f689768237dbb9dabe4f7841771
Kristoffer Walker <kixxauth@gmail.com>
2010-01-20 14:55:07 -0500

    authentication built into testrunner.py
     On branch test_protocol
    	modified:   test/testrunner.py

--- test/testrunner.py
+++ test/testrunner.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 import sys
 import os
+import httplib
+import simplejson
+import yaml
 
 import suites
 from tests import test_utils
-import httplib
-import yaml
 
 def checkhost(url):
   cxn = httplib.HTTPConnection(url)
@@ -20,6 +21,73 @@ def getconfigs(dir):
   configs as parsed by PyYaml.
   """
   return yaml.load(open(os.path.join(dir, 'app.yaml')))
+
+def make_auth_request(host, username, passkey=None, nonce=None, nextnonce=None):
+  if nonce is None or nextnonce is None or passkey is None:
+    creds = '["%s"]'% username
+  else:
+    creds = simplejson.dumps(
+        test_utils.create_credentials(
+          passkey, username, nonce, nextnonce))
+
+  body = '{"head":{"method":"get", "authorization":%s}}'% creds
+  
+  cxn = httplib.HTTPConnection(host)
+  cxn.request('POST', '/', body, {
+        'User-Agent': 'UA:DCube test :: Auth sys-admin',
+        'Content-Length': str(len(body)),
+        'Accept': 'application/jsonrequest',
+        'Content-Type': 'application/jsonrequest'})
+  response = cxn.getresponse()
+  response_body = response.read()
+  cxn.close()
+
+  assert response.status is 200, \
+      'Unexpected HTTP status code (%d) on login.'% response.status
+  json = simplejson.loads(response_body)
+
+  len_auth = len(json['head']['authorization'])
+  if len_auth < 1:
+    return False, None, None, None
+
+  if len_auth is 3:
+    auth = json['head']['status'] is 200 and True or False
+    nonce = json['head']['authorization'][1]
+    nextnonce = json['head']['authorization'][2]
+    return auth, username, nonce, nextnonce
+
+  return False, username, None, None
+
+def prompt_username():
+  un = raw_input('sys-admin username: ')
+  if len(un) < 1:
+    print 'username must be more than 0 characters'
+    return prompt_username()
+  return un
+
+def prompt_passkey(username):
+  import getpass
+  pk = getpass.getpass('passkey for %s: '% username)
+  if len(pk) < 1:
+    print 'passkey must be more than 0 characters'
+    return prompt_passkey()
+  return pk
+
+def authenticate(host):
+  username = prompt_username()
+  auth, username_, nonce, nextnonce = make_auth_request(host, username)
+  if username_ is None:
+    return None, None
+
+  assert nonce and nextnonce, 'Missing nonce or nextnonce.'
+  passkey = prompt_passkey(username)
+  auth, username_, nonce, nextnonce = \
+      make_auth_request(host, username, passkey, nonce, nextnonce)
+
+  if not auth:
+    return username, None
+
+  return username, passkey
 
 def main():
   localhost = 'localhost:8080'
@@ -47,6 +115,13 @@ def main():
 
   elif checkhost(remote_host):
     host = remote_host
+    temp_test_admin, passkey = authenticate(host)
+    if temp_test_admin is None:
+      print 'User does not exist.'
+      exit()
+    if passkey is None:
+      print 'Invalid passkey... exit()'
+      exit()
 
   else:
     raise Exception('no connection to %s or %s'% (localhost, remote_host))
@@ -60,17 +135,18 @@ def main():
   print ''
   print 'Running tests on: %s' % host 
   print 'Running suites: %s' % suites_
-  print 'Using admin: %s'% temp_test_admin
+  print 'Using admin account for: %s'% temp_test_admin
   print ''
 
   suites.run_suites(suites_)
 
   # If you remove this bit of functionality, I will shoot you.
-  cxn = httplib.HTTPConnection(localhost)
-  cxn.request('DELETE', '/testsetup')
-  response = cxn.getresponse()
-  assert response.status == 204, \
-      'TEST USER WAS NOT DELETED (http status:%d)'% response.status
+  if host is localhost:
+    cxn = httplib.HTTPConnection(localhost)
+    cxn.request('DELETE', '/testsetup')
+    response = cxn.getresponse()
+    assert response.status == 204, \
+        'TEST USER WAS NOT DELETED (http status:%d)'% response.status
 
 if __name__ == '__main__':
   main()
